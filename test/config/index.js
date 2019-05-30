44c8b3d2bc6af76fb090feb5dc5475b7f3bb832b
233zhao <2926093155@qq.com>
2019-05-30 19:45:25 +0800

    重置vue-cli

--- test/config/index.js
+++ test/config/index.js
@@ -10,15 +10,7 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-		'/apis':{
-			target: 'http://localhost:8080',
-			changeOrigin: true,
-			pathRewrite:{
-				'^/apis':'/'
-			}
-		}
-	},
+    proxyTable: {},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
@@ -28,7 +20,14 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+    // Use Eslint Loader?
+    // If true, your code will be linted during bundling and
+    // linting errors and warnings will be shown in the console.
+    useEslint: true,
+    // If true, eslint errors and warnings will also be shown in the error overlay
+    // in the browser.
+    showEslintErrorsInOverlay: false,
+
     /**
      * Source Maps
      */
--- test/src/router/index.js
+++ test/src/router/index.js
@@ -1,31 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import app from '@/components/xhApp.vue'
-import index from '@/components/app/index.vue'
-import lists from '@/components/app/list.vue'
-import sc from '@/components/app/sancan.vue'
+import HelloWorld from '@/components/HelloWorld'
+
 Vue.use(Router)
+
 export default new Router({
   routes: [
-	{
-		path:'/',
-		name:'index',
-		component:index
-	},
-	{
-		path:'/lists',
-		name:'lists',
-		component:lists
-	},
-	{
-		path:'/sancan',
-		name:'sancan',
-		component:sc
-	},
-	{
-		path:'/app',
-		name:'download',
-		component:app
-	}
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
   ]
 })
