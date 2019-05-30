3475d3a14f7e3062a0d843fc8609b2051908788e
233zhao <2926093155@qq.com>
2019-05-30 19:29:24 +0800

    删除无效组件

---
+++ test/src/pages/loding/loadin.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="loadin"></div>
+</template>
+<script>
+export default {
+  name: 'loadin'
+}
+</script>
+<style lang="stylus" scoped>
+.loadin{
+    background: #eee;
+}
+</style>
+
