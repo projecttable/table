2aa66bf0b9140b905c78bf2fcdb4c47a2e091a5a
gaoxiaoxiao <1948475178@qq.com>
2019-05-31 09:31:02 +0800

    更新index.js

--- test/src/router/index.js
+++ test/src/router/index.js
@@ -1,39 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
-// import index from '@/pages/header'
-// import left from '@/pages/left'
 import main from '@/components/main'
-=======
->>>>>>> 63886099d8e9b7a8506d7d6d3b4c09c02bc08d38
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'index',
-    //   component: index
-    // },
-    // {
-    //   path: '/left',
-    //   name: 'left',
-    //   component: left
-    // }
     {
       path: '/',
-<<<<<<< HEAD
       name: 'main',
       component: main
-=======
-      redirect: '/table'
-    },
-    {
-      path: '/table',
-      name: 'table',
-      component: res => require(['@/pages/table'], res)
->>>>>>> 63886099d8e9b7a8506d7d6d3b4c09c02bc08d38
     }
   ]
 })
