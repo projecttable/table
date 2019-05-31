5bae9a5dc237cb82e3462433626312fe160c8f78
gaoxiaoxiao <1948475178@qq.com>
2019-05-31 09:28:35 +0800

    更新main.js

--- test/src/main.js
+++ test/src/main.js
@@ -3,17 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-<<<<<<< HEAD
 import '@/assets/css/reset.styl'
 import ele from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ele)
-=======
-
-import elementUi from 'element-ui'
-Vue.use(elementUi)
-
->>>>>>> 63886099d8e9b7a8506d7d6d3b4c09c02bc08d38
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
